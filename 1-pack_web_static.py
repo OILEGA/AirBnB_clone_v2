87b0819bbfd3820dfb2e9076cfe1b232595ca7c7
OILEGA <get2mena@gmail.com>
2023-07-11 10:44:47 +0000

    Airbnb static deploy project

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
+import os
 from datetime import datetime
 from fabric.api import local
 
@@ -15,7 +15,7 @@ def do_pack():
                                                          dt.minute,
                                                          dt.second)
     if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
+        if local("mkdir versions").failed is True:
             return None
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
